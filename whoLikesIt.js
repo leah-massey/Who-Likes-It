18bd7aa8145c8cd57f539fa0b3f2407e3fc2ca6f
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-06-20 09:49:21 +0100

    Test 2 passing: correct result when two names like a post.

--- whoLikesIt.js
+++ whoLikesIt.js
@@ -1,5 +1,9 @@
 const likes = function (names) {
-  return `${names} likes this`;
+  if (names.length === 1) {
+    return `${names} likes this`;
+  } else {
+    return `${names[0]} and ${names[1]} like this`;
+  }
 };
 
 module.exports = likes;
--- whoLikesIt.test.js
+++ whoLikesIt.test.js
@@ -4,4 +4,8 @@ describe("likes", () => {
   it("returns the name of the person who likes the post", () => {
     expect(likes(["James"])).toEqual("James likes this");
   });
+
+  it("returns the name of the people who likes the post", () => {
+    expect(likes(["Jacob", "Alex"])).toEqual("Jacob and Alex like this");
+  });
 });
