7e4e50490e3d63650098650e74a2b5e8e5fde393
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-06-20 16:35:37 +0100

    Refactoring of code using switch statement.

--- whoLikesIt.js
+++ whoLikesIt.js
@@ -3,16 +3,19 @@ const likes = function (names) {
     throw new Error("Input not in correct format.");
   }
 
-  if (names.length === 0) {
-    return "no one likes this";
-  } else if (names.length === 1) {
-    return `${names} likes this`;
-  } else if (names.length === 2) {
-    return `${names[0]} and ${names[1]} like this`;
-  } else if (names.length === 3) {
-    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-  } else {
-    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${names} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${
+        names.length - 2
+      } others like this`;
   }
 };
 
